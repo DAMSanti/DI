98477ab280ff77925d29350ea5843485aa257d6c
Santiago Manuel Tamayo Arozamena <145398719+DAMSanti@users.noreply.github.com>
2024-10-04 20:11:02 +0200

    .

---
+++ C#/Unidad02/Hoja03/Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,150 @@
+﻿using System.ComponentModel.Design;
+
+public class Figuras {
+    double area;
+    double perimetro;
+
+    public Figuras() {
+    }
+    public virtual double CalcularArea() {
+       return area;
+    }
+    public virtual double CalcularPerimetro() {
+       return perimetro;
+    }
+}
+
+class Rectangulo : Figuras {
+    public double baseRectangulo { set; get; }
+    public double alturaRectangulo { set; get; }
+
+    public override double CalcularArea() {
+        return baseRectangulo * alturaRectangulo;
+    }
+    public override double CalcularPerimetro() {
+        return baseRectangulo*2 + alturaRectangulo*2;
+    }
+}
+
+class  Triangulo : Figuras {
+    double baseTriangulo { set; get; }
+    double alturaTriangulo { set; get;}
+    double[] ladosTriangulo { set; get; } = new double[3];
+
+    public override double CalcularArea() {
+        return (baseTriangulo * alturaTriangulo) / 2;
+    }
+    public override double CalcularPerimetro() {
+        return ladosTriangulo[0]+ladosTriangulo[1]+ladosTriangulo[2];
+    }
+}
+
+class Cuadrado : Figuras {
+    double ladoCuadrado { set; get;}
+
+    public override double CalcularArea() {
+        return ladoCuadrado * ladoCuadrado;
+    }
+    public override double CalcularPerimetro() {
+        return ladoCuadrado * 4;
+    }
+}
+class Rombo : Figuras {
+    double diagonalMayor { set; get; }
+    double diagonalMenor { set; get; }
+    public void setDiagonalMayor(double diagonalMayor) {
+        this.diagonalMayor = diagonalMayor;
+    }
+    public void setDiagonalMenor(double diagonalMenor) {
+        this.diagonalMenor = diagonalMenor;
+    }
+    public override double CalcularArea() {
+        return (diagonalMayor * diagonalMenor) / 2;
+    }
+
+}
+class Trapecio : Figuras {
+    double baseMayor;
+    double baseMenor;
+    double alturaTrapecio;
+    public override double CalcularArea() {
+        return ((baseMayor + baseMenor) * alturaTrapecio) / 2;
+    }
+    public override double CalcularPerimetro() {
+        return baseMayor + baseMenor + alturaTrapecio;
+    }
+}
+
+public class Programa {
+    public static void Main(string[] args) {
+        bool salir = false;
+        while (!salir) {
+            string opcion = Menu();
+            switch (opcion) {
+                case "1":
+                    int valor = int.Parse(Menu2());
+                    var rectangulo = new Rectangulo();
+                    if (valor == 1) {
+                        Console.WriteLine("Introduce la base del rectangulo: ");
+                        rectangulo.SetBaseRectangulo(int.Parse(Console.ReadLine()));
+                        Console.WriteLine("Introduce la altura del rectangulo: ");
+                        rectangulo.SetAlturaRectangulo(int.Parse(Console.ReadLine()));
+                        Console.WriteLine(rectangulo.CalcularArea());
+                    } else if (valor == 2) {
+                        Console.WriteLine("Introduce la base del rectangulo: ");
+                        rectangulo.SetBaseRectangulo(int.Parse(Console.ReadLine()));
+                        Console.WriteLine("Introduce la altura del rectangulo: ");
+                        rectangulo.SetAlturaRectangulo(int.Parse(Console.ReadLine()));
+                        Console.WriteLine(rectangulo.CalcularPerimetro);
+                    } else {
+                        Console.WriteLine("Atrás");
+                        break;
+                    }
+                    break;
+                case "2":
+                    Menu2();
+                    var triangulo = new Triangulo();
+                    Console.WriteLine(triangulo.CalcularArea());
+                    break;
+                case "3":
+                    Menu2();
+                    var cuadrado = new Cuadrado();
+                    Console.WriteLine(cuadrado.CalcularArea());
+                    break;
+                case "4":
+                    Menu2();
+                    var rombo = new Rombo();
+                    Console.WriteLine(rombo.CalcularArea());
+                    break;
+                case "5":
+                    Menu2();
+                    var trapecio = new Trapecio();
+                    Console.WriteLine(trapecio.CalcularArea());
+                    break;
+                case "6":
+                    salir = true;
+                    break;
+
+            }
+
+        }
+
+    }
+    public static string Menu() {
+        Console.WriteLine("CALCULADORA DE FIGURAS\n\t1. Rectangulo\n\t2. Triangulo\n\t3. Cuadrado\n\t4. Rombo\n\t5. Trapecio\n\t6. Salir\n\nElige una opcion: ");
+        return Console.ReadLine();
+    }
+
+    public static String Menu2() {
+        Console.WriteLine("CALCULADORA DE FIGURAS\n\t1. Calcular Area\n\t2. Calcular Perimetro\n\t3. Atrás\n\nElige una opcion: ");
+        return Console.ReadLine();
+    }
+}
+
+
+
+/*
+ * 
+ * 1. Crea una clase Figuras y varias clases derivadas para calcular el área y el perímetro de rectángulo, triángulo, cuadrado, rombo y trapecio.
+ * 
+ */
--- C#/Unidad02/Hoja03/Ejercicio01/Ejercicio01/obj/Debug/net8.0/Ejercicio01.AssemblyInfo.cs
+++ C#/Unidad02/Hoja03/Ejercicio01/Ejercicio01/obj/Debug/net8.0/Ejercicio01.AssemblyInfo.cs
@@ -11,15 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af56605a60943cc98993116102a80cec922b6a3c")]
-[assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio01")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
